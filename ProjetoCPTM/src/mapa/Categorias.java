918105eb8f1ee514031ff57cf908a98812406006
Leonardo Moret de Oliveira <lmoret0410@gmail.com>
2024-03-25 09:14:29 -0300

    get

--- ProjetoCPTM/src/mapa/Categorias.java
+++ ProjetoCPTM/src/mapa/Categorias.java
@@ -11,6 +11,31 @@ public class Categorias {
 		this.tipo = tipo;
 		this.descricao = descricao;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	
 	//getters e setters
+	
 }

ada674f078c8cd85676a183f2e3a26a41e99cb14
stefanom24 <stefanom24@gmail.com>
2024-04-01 08:57:10 -0300

    Utils para json criados

--- cptm/src/main/java/com/projeto/cptm/cptm/utils/utils.java
+++ cptm/src/main/java/com/projeto/cptm/cptm/utils/utils.java
@@ -1,24 +1,46 @@
 package com.projeto.cptm.cptm.utils;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.cptm.cptm.Login;
+
+
 
 @RestController
 @RequestMapping("/login")
 public class utils {
     private String usuario;
     private String password;
+    private Login login;
 
-    private void lerLogin(String usuario, String password){
+    private void lerLogin(String usuario, String password, Login login){
         this.usuario = usuario;
         this.password = password;
+        this.login = login;
     }
 
-    @GetMapping("/login:{user}{pass}")
-    public boolean ler(@PathVariable(name = "user", pass = "pass") String user, String pass){
-        
+    @GetMapping("login/{user}/{pass}") 
+    public void ler(@PathVariable("user") String user, @PathVariable("pass") String pass){
+        boolean check = verificar(login.getUser(), login.getPass());
+        if(check == true){
+            //direciona/chama alguma coisa
+        }else{
+            //criar ou direcionar msg de erro
+        }
+    }
+
+    //criar metodo que vai definir um local pra ler e salvar json
+    public boolean verificar(String usuario, String password){
+        //verificar se usuario e senha recebido e igual do arquivo json
         return true;
     }
+
+    //
+
 }
